ebddea8d2cffc675358eb3de2c44c38f56ebee9a
mizmiya <kattyan.08ms@gmail.com>
2018-02-12 18:52:29 +0900

    ユーザー画像実装

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -5,7 +5,8 @@ class Blog < ApplicationRecord
 
   def self.search(search) #self.でクラスメソッドとしている
     if search # Controllerから渡されたパラメータが!= nilの場合は、titleカラムを部分一致検索
-      Blog.where(['content LIKE ?', "%#{search}%"])
+      Blog.joins(:user).where(["(title LIKE ?) OR (content LIKE ?) OR (content2 LIKE ?) OR (users.name LIKE ?)",
+         "%#{search}%", "%#{search}%", "%#{search}%", "%#{search}%"])
     else
       Blog.all #全て表示。
     end
